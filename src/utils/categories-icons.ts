9e8a43f81874b87a598c1ab0ce131f319b269f6e
Herley Oliveira <herley.oliveira@bancoartico.com>
2025-01-16 21:27:00 -0300

    feat: add font family and install package to icons and svg

---
+++ src/utils/categories-icons.ts
@@ -0,0 +1,16 @@
+import {
+    IconBed,
+    IconMovie,
+    IconCoffee,
+    IconShoppingBag,
+    IconToolsKitchen2,
+    IconProps,
+} from "@tabler/icons-react-native"
+
+export const categoriesIcons: Record<string, React.ComponentType<IconProps>> = {
+    "": IconToolsKitchen2,
+    "": IconShoppingBag,
+    "": IconBed,
+    "": IconMovie,
+    "": IconCoffee,
+}
